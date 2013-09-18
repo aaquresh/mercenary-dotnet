39cf3c73e3d14e3f465b972ffd2c42ee525c9e87
cnimnicht <c_nimnicht@hotmail.com>
2013-09-18 13:24:10 -0600

    Adding in code for interfaces
    Added code for interfaces, renamed class1.cs to Interfaces.cs.
    
    This library will be used for inheritance in the framework as well as
    plugins

---
+++ PluginFramework/Interfaces/Interfaces.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+
+    //Abstract Factory Interface
+    public interface IAbstractTestToolFactory
+    {
+        IAbstractTestCaseProduct CreateTestCase(string FactoryTestCase, string TestCaseID, string SessionID);
+    }
+
+    //Abstract Product Interface
+    public interface IAbstractTestCaseProduct
+    {
+        void Run(string TestCaseID, string SessionID, string args);
+    }
+}
