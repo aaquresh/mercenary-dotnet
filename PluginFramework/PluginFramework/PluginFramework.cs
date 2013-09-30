97316fe4a549d32382baad0dddb5c64789201564
cnimnicht <c_nimnicht@hotmail.com>
2013-09-29 23:40:45 -0600

    updates to plugin framework
    Added .json config file to store info about plugins

--- PluginFramework/PluginFramework/PluginFramework.cs
+++ PluginFramework/PluginFramework/PluginFramework.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using Interfaces;
 using Newtonsoft.Json.Linq;
 using System.Reflection;
@@ -13,15 +14,38 @@ namespace PluginFramework
     {
         static public string RunTest(string Json)
         {
+            string pluginName;
+            string pluginPath;
+            string pluginNamespace;
+
             //Pull test tool info from json config string
             try
             {
                 JObject o = JObject.Parse(Json);
 
-
                 string testTool = (string)o["tasks"][0]["plugin"];
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+
+            //Pull test tool info from local config file
+
+            try
+            {
+                StreamReader sr = new StreamReader("config.json");
+
+                string config = sr.ReadToEnd();
+
+                JObject jConfig = JObject.Parse(config);
+
+                //Need to fix this so that it cycles through possible plugins
 
 
+                pluginName = (string)jConfig["plugin"][0]["name"];
+                pluginPath = (string)jConfig["plugin"][0]["dllLocation"];
+                pluginNamespace = (string)jConfig["plugin"][0]["namespace"];
 
             }
             catch (Exception ex)
@@ -30,14 +54,16 @@ namespace PluginFramework
             }
 
 
-            Assembly assembly = Assembly.LoadFrom(@"C:\Users\cmnimnic\Documents\GitHub\mercenary-dotnet\Plugins\SoapUIPlugin\bin\Debug\SoapUIPlugin.dll");
 
-            Type type = assembly.GetType("SoapUIPlugin.SoapUIPlugin");
+
+            Assembly assembly = Assembly.LoadFrom(pluginPath);
+
+            Type type = assembly.GetType(pluginNamespace + "." + pluginName);
 
             IAbstractTestToolFactory instance = Activator.CreateInstance(type) as IAbstractTestToolFactory;
 
             IAbstractTestCaseProduct tcp = instance.CreateTestCase(Json);
-
+            
             tcp.Run();
 
 
