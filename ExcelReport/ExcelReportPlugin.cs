f0787c6ef9994007767465477ee4eb2029db5bb1
cnimnicht <c_nimnicht@hotmail.com>
2013-10-23 10:47:14 -0600

    Updates to Report Request object, unit tests

--- ExcelReport/ExcelReportPlugin.cs
+++ ExcelReport/ExcelReportPlugin.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using Newtonsoft.Json.Linq;
@@ -10,16 +10,108 @@ namespace ExcelReport
     {
     }
 
-    public class ReportRequest
+    public class ReportRequest : IEnumerable
     {
         //fields
         TestCase[] testCases;
-        //constructor
+        string error;
+        string testSuite;
+        int testSuiteID;
+        DateTime testSuiteStartDate;
+        DateTime testSuiteEndDate;
+
+        //constructor - will refactor latter
         public ReportRequest(string jsonRequest)
         {
             JObject jConfig = JObject.Parse(jsonRequest);
 
+            //set Test Suite
+            testSuite = (string)jConfig["testsuite"];
+            if (testSuite == "")
+            {
+                error = "Test Suite is not set to a value.";
+                return;
+            }
+            if (testSuite == null)
+            {
+                error = "No test suite present in the json request file.";
+                return;
+            }
+
+            //set Test Suite ID
+            string tempTestSuiteID = (string)jConfig["testsuiteID"];
+            if (tempTestSuiteID == "")
+                tempTestSuiteID = "0";
+            try
+            {
+                testSuiteID = Convert.ToInt16(tempTestSuiteID);
+            }
+            catch 
+            {
+                error = "Test Suite ID is not set to an integer.";
+                return;
+            }
+
+            //set Test Suite Start Date
+            string tempTestSuiteStartDate = (string)jConfig["testsuitestartdate"];
+            if (tempTestSuiteStartDate != null && tempTestSuiteStartDate != "")
+            {
+                try
+                {
+                    testSuiteStartDate = Convert.ToDateTime(tempTestSuiteStartDate);
+                }
+                catch
+                {
+                    error = "Test Suite Start Date is not set to a DateTime.";
+                    return;
+                }
+            }
+
+            //set Test Suite End Date
+            string tempTestSuiteEndDate = (string)jConfig["testsuiteenddate"];
+            if (tempTestSuiteEndDate != null && tempTestSuiteEndDate != "")
+            {
+                try
+                {
+                    testSuiteEndDate = Convert.ToDateTime(tempTestSuiteEndDate);
+                }
+                catch
+                {
+                    error = "Test Suite End Date is not set to a DateTime.";
+                    return;
+                }
+            }
+
+            //Add TestCase to Report Request Object
+            JArray Jray = (JArray)jConfig["testcases"];
+
+            int count = Jray.Count;
+            int position = 0;
+
+            testCases = new TestCase[count];
+
+            //If there are no test cases in the json request.
+            //Exit and set error to "No test cases present in the json request file."
+            if (count == 0)
+            {
+                error = "No test cases present in the json request file.";
+                return;
+            }
+
+            while (position < count)
+            {
+
+                JToken jt = Jray[position];
+
+
+                TestCase testCase = new TestCase(Jray[position].ToString());
+                testCases[position] = testCase;
+                position++;
+            }
+
         }
+
+
         //properties
         public TestCase[] TestCases
         {
@@ -28,10 +120,164 @@ namespace ExcelReport
                 return testCases;
             }
         }
+        public string Error
+        {
+            get
+            {
+                return error;
+            }
+        }
+        public string TestSuite
+        {
+            get
+            {
+                return testSuite;
+            }
+        }
+        public int TestSuiteID
+        {
+            get
+            {
+                return testSuiteID;
+            }
+        }
+        public DateTime TestSuiteStartDate
+        {
+            get
+            {
+                return testSuiteStartDate;
+            }
+        }
+        public DateTime TestSuiteEndDate
+        {
+            get
+            {
+                return testSuiteEndDate;
+            }
+        }
+        public int NumberofTests
+        {
+            get
+            {
+                return TestCases.Count();
+            }
+        }
 
+        //Enumerator Code
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)GetEnumerator();
+        }
+
+        public ReportRequestEnum GetEnumerator()
+        {
+            return new ReportRequestEnum(testCases);
+        }
+    }
+
+    public class ReportRequestEnum : IEnumerator
+    {
+        public TestCase[] testCases;
+        private int position = -1;
+
+        public ReportRequestEnum(TestCase[] testCases)
+        {
+            this.testCases = testCases;
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+            return (position < testCases.Length);
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        object IEnumerator.Current
+        {
+            get { return Current; }
+        }
+
+        public TestCase Current
+        {
+            get
+            {
+                try
+                {
+                    return testCases[position];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
     }
 
     public class TestCase
     {
+        //fields
+        TestParameters testParameters;
+        string error;
+        //constructor
+        public TestCase(string jsonTestCase)
+        {
+            JObject jConfig = JObject.Parse(jsonTestCase);
+
+            JToken jsonParameters = jConfig["parameters"];
+
+            //Instantiate Test Parameters object, check to see if it's null and throw error
+
+            if (jsonParameters == null)
+            {
+                error = "Test Case is missing Test Parameters, this test will not be included in the report.";
+                return;
+            }
+
+            JToken jsonTestParameters = jsonParameters["test"];
+            testParameters = new TestParameters(jsonTestParameters.ToString());
+
+
+            JToken jsonOptionsParameters = jsonParameters["options"];
+            
+
+        }
+        //properties
+        public TestParameters TestParameters
+        {
+            get
+            {
+                return testParameters;
+            }
+        }
+        public string Error
+        {
+            get
+            {
+                return error;
+            }
+        }
     }
+
+
+    public class TestParameters
+    {
+        //fields
+        string[,] test;
+        string[,] options;
+        //constructor
+        public TestParameters(string jsonRequest)
+        {
+
+        }
+        //properties
+
+    }
+
+    
+
+
 }
--- FrameworkTests/ReportFrameworkTests.cs
+++ FrameworkTests/ReportFrameworkTests.cs
@@ -14,6 +14,10 @@ namespace FrameworkTests
     [TestClass]
     public class ReportFrameworkTests
     {
+        /// <summary>
+        /// Test Case unit tests
+        /// </summary>
+
         [TestMethod]
         [DeploymentItem(@"FrameworkTests\JsonTestFiles\ReportRequest_TestCase_Populates.json")]
         public void ReportRequest_TestCase_Populates()
@@ -25,20 +29,306 @@ namespace FrameworkTests
             Assert.IsTrue(reportRequest.TestCases.Count() > 0);
         }
 
-
-
-
-
-
         [TestMethod]
-        [DeploymentItem(@"FrameworkTests\JsonTestFiles\FrameworkTests_ReadJsonFile_ValidFile.json")]
-        public void FrameworkTests_ReadJsonFile_ValidFile()
+        [DeploymentItem(@"FrameworkTests\JsonTestFiles\ReportRequest_TestCase_Missing.json")]
+        public void ReportRequest_TestCase_Missing()
         {
-            string jsonFile = ReadJsonFile("FrameworkTests_ReadJsonFile_ValidFile.json");
-            Assert.AreNotEqual(jsonFile, "");
+            string jsonRequest = ReadJsonFile("ReportRequest_TestCase_Missing.json");
+
+            ReportRequest reportRequest = new ReportRequest(jsonRequest);
+
+            Assert.IsTrue(reportRequest.TestCases.Count() == 0);
+            Assert.IsTrue(reportRequest.Error == "No test cases present in the json request file.");
         }
 
+        /// <summary>
+        /// Test Suite unit tests
+        /// </summary>
+
+        [TestMethod]
+        [DeploymentItem(@"FrameworkTests\JsonTestFiles\ReportRequest_TestSuite_Exists.json")]
+        public void ReportRequest_TestSuite_Exists()
+        {
+            string jsonRequest = ReadJsonFile("ReportRequest_TestSuite_Exists.json");
+
+            ReportRequest reportRequest = new ReportRequest(jsonRequest);
+
+            Assert.IsTrue(reportRequest.TestCases.Count() > 0);
+            Assert.IsFalse(reportRequest.TestSuite == "");
+            Assert.IsNotNull(reportRequest.TestSuite);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"FrameworkTests\JsonTestFiles\ReportRequest_TestSuite_Missing.json")]
+        public void ReportRequest_TestSuite_Missing()
+        {
+            string jsonRequest = ReadJsonFile("ReportRequest_TestSuite_Missing.json");
+
+            ReportRequest reportRequest = new ReportRequest(jsonRequest);
+
+            Assert.IsTrue(reportRequest.TestSuite == null);
+            Assert.IsTrue(reportRequest.TestCases == null);
+            Assert.IsTrue(reportRequest.Error == "No test suite present in the json request file.");
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"FrameworkTests\JsonTestFiles\ReportRequest_TestSuite_Blank.json")]
+        public void ReportRequest_TestSuite_Blank()
+        {
+            string jsonRequest = ReadJsonFile("ReportRequest_TestSuite_Blank.json");
+
+            ReportRequest reportRequest = new ReportRequest(jsonRequest);
+
+            Assert.IsTrue(reportRequest.TestSuite == "");
+            Assert.IsTrue(reportRequest.TestCases == null);
+            Assert.IsTrue(reportRequest.Error == "Test Suite is not set to a value.");
+        }
+
+        /// <summary>
+        /// Test SuiteID unit tests
+        /// </summary>
+
+        [TestMethod]
+        [DeploymentItem(@"FrameworkTests\JsonTestFiles\ReportRequest_TestSuiteID_Exists.json")]
+        public void ReportRequest_TestSuiteID_Exists()
+        {
+            string jsonRequest = ReadJsonFile("ReportRequest_TestSuiteID_Exists.json");
+
+            ReportRequest reportRequest = new ReportRequest(jsonRequest);
+
+            Assert.IsTrue(reportRequest.TestCases.Count() > 0);
+            Assert.IsTrue(reportRequest.TestSuiteID > 0);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"FrameworkTests\JsonTestFiles\ReportRequest_TestSuiteID_Missing.json")]
+        public void ReportRequest_TestSuiteID_Missing()
+        {
+            string jsonRequest = ReadJsonFile("ReportRequest_TestSuiteID_Missing.json");
+
+            ReportRequest reportRequest = new ReportRequest(jsonRequest);
+
+            Assert.IsTrue(reportRequest.TestSuiteID == 0);
+            Assert.IsTrue(reportRequest.TestCases.Count() > 0);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"FrameworkTests\JsonTestFiles\ReportRequest_TestSuiteID_Blank.json")]
+        public void ReportRequest_TestSuiteID_Blank()
+        {
+            string jsonRequest = ReadJsonFile("ReportRequest_TestSuiteID_Blank.json");
+
+            ReportRequest reportRequest = new ReportRequest(jsonRequest);
+
+            Assert.IsTrue(reportRequest.TestSuiteID == 0);
+            Assert.IsTrue(reportRequest.TestCases.Count() > 0);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"FrameworkTests\JsonTestFiles\ReportRequest_TestSuiteID_NonInt.json")]
+        public void ReportRequest_TestSuiteID_NonInt()
+        {
+            string jsonRequest = ReadJsonFile("ReportRequest_TestSuiteID_NonInt.json");
+
+            ReportRequest reportRequest = new ReportRequest(jsonRequest);
+
+            Assert.IsTrue(reportRequest.TestSuiteID == 0);
+            Assert.IsTrue(reportRequest.TestCases == null);
+            Assert.IsTrue(reportRequest.Error == "Test Suite ID is not set to an integer.");
+        }
+
+
+        /// <summary>
+        /// Test TestSuiteStartDate unit tests
+        /// </summary>
+
+        [TestMethod]
+        [DeploymentItem(@"FrameworkTests\JsonTestFiles\ReportRequest_TestSuiteStartDate_Exists.json")]
+        public void ReportRequest_TestSuiteStartDate_Exists()
+        {
+            string jsonRequest = ReadJsonFile("ReportRequest_TestSuiteStartDate_Exists.json");
+
+            ReportRequest reportRequest = new ReportRequest(jsonRequest);
+
+            Assert.IsTrue(reportRequest.TestCases.Count() > 0);
+
+            DateTime dt = new DateTime();
+
+            Assert.IsTrue(dt != reportRequest.TestSuiteStartDate);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"FrameworkTests\JsonTestFiles\ReportRequest_TestSuiteStartDate_Missing.json")]
+        public void ReportRequest_TestSuiteStartDate_Missing()
+        {
+            string jsonRequest = ReadJsonFile("ReportRequest_TestSuiteStartDate_Missing.json");
+
+            ReportRequest reportRequest = new ReportRequest(jsonRequest);
+
+            DateTime dt = new DateTime();
+
+            Assert.IsTrue(dt == reportRequest.TestSuiteStartDate);
+            Assert.IsTrue(reportRequest.TestCases.Count() > 0);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"FrameworkTests\JsonTestFiles\ReportRequest_TestSuiteStartDate_Blank.json")]
+        public void ReportRequest_TestSuiteStartDate_Blank()
+        {
+            string jsonRequest = ReadJsonFile("ReportRequest_TestSuiteStartDate_Blank.json");
+
+            ReportRequest reportRequest = new ReportRequest(jsonRequest);
+
+            DateTime dt = new DateTime();
+
+            Assert.IsTrue(reportRequest.TestSuiteStartDate == dt);
+            Assert.IsTrue(reportRequest.TestCases.Count() > 0);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"FrameworkTests\JsonTestFiles\ReportRequest_TestSuiteStartDate_NonDateTime.json")]
+        public void ReportRequest_TestSuiteStartDate_NonDateTime()
+        {
+            string jsonRequest = ReadJsonFile("ReportRequest_TestSuiteStartDate_NonDateTime.json");
+
+            ReportRequest reportRequest = new ReportRequest(jsonRequest);
+
+            DateTime dt = new DateTime();
+
+            Assert.IsTrue(reportRequest.TestSuiteStartDate == dt);
+            Assert.IsTrue(reportRequest.TestCases == null);
+            Assert.IsTrue(reportRequest.Error == "Test Suite Start Date is not set to a DateTime.");
+        }
+
+
+        /// <summary>
+        /// Test TestSuiteEndDate unit tests
+        /// </summary>
+
+        [TestMethod]
+        [DeploymentItem(@"FrameworkTests\JsonTestFiles\ReportRequest_TestSuiteEndDate_Exists.json")]
+        public void ReportRequest_TestSuiteEndDate_Exists()
+        {
+            string jsonRequest = ReadJsonFile("ReportRequest_TestSuiteEndDate_Exists.json");
+
+            ReportRequest reportRequest = new ReportRequest(jsonRequest);
+
+            Assert.IsTrue(reportRequest.TestCases.Count() > 0);
+
+            DateTime dt = new DateTime();
+
+            Assert.IsTrue(dt != reportRequest.TestSuiteEndDate);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"FrameworkTests\JsonTestFiles\ReportRequest_TestSuiteEndDate_Missing.json")]
+        public void ReportRequest_TestSuiteEndDate_Missing()
+        {
+            string jsonRequest = ReadJsonFile("ReportRequest_TestSuiteEndDate_Missing.json");
+
+            ReportRequest reportRequest = new ReportRequest(jsonRequest);
+
+            DateTime dt = new DateTime();
+
+            Assert.IsTrue(dt == reportRequest.TestSuiteEndDate);
+            Assert.IsTrue(reportRequest.TestCases.Count() > 0);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"FrameworkTests\JsonTestFiles\ReportRequest_TestSuiteEndDate_Blank.json")]
+        public void ReportRequest_TestSuiteEndDate_Blank()
+        {
+            string jsonRequest = ReadJsonFile("ReportRequest_TestSuiteEndDate_Blank.json");
+
+            ReportRequest reportRequest = new ReportRequest(jsonRequest);
+
+            DateTime dt = new DateTime();
+
+            Assert.IsTrue(reportRequest.TestSuiteEndDate == dt);
+            Assert.IsTrue(reportRequest.TestCases.Count() > 0);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"FrameworkTests\JsonTestFiles\ReportRequest_TestSuiteEndDate_NonDateTime.json")]
+        public void ReportRequest_TestSuiteEndDate_NonDateTime()
+        {
+            string jsonRequest = ReadJsonFile("ReportRequest_TestSuiteEndDate_NonDateTime.json");
+
+            ReportRequest reportRequest = new ReportRequest(jsonRequest);
+
+            DateTime dt = new DateTime();
+
+            Assert.IsTrue(reportRequest.TestSuiteEndDate == dt);
+            Assert.IsTrue(reportRequest.TestCases == null);
+            Assert.IsTrue(reportRequest.Error == "Test Suite End Date is not set to a DateTime.");
+        }
+
+        /// <summary>
+        /// Test NumberofTests unit tests
+        /// </summary>
+
+        [TestMethod]
+        [DeploymentItem(@"FrameworkTests\JsonTestFiles\ReportRequest_NumberofTests_Populates1.json")]
+        [DeploymentItem(@"FrameworkTests\JsonTestFiles\ReportRequest_NumberofTests_Populates5.json")]
+        [DeploymentItem(@"FrameworkTests\JsonTestFiles\ReportRequest_NumberofTests_Populates10.json")]
+        public void ReportRequest_NumberofTests_Populates()
+        {
+            string jsonRequest1 = ReadJsonFile("ReportRequest_NumberofTests_Populates1.json");
+            string jsonRequest2 = ReadJsonFile("ReportRequest_NumberofTests_Populates5.json");
+            string jsonRequest3 = ReadJsonFile("ReportRequest_NumberofTests_Populates10.json");
+
+            ReportRequest reportRequest1 = new ReportRequest(jsonRequest1);
+            ReportRequest reportRequest2 = new ReportRequest(jsonRequest2);
+            ReportRequest reportRequest3 = new ReportRequest(jsonRequest3);
+
+            Assert.IsTrue(reportRequest1.TestCases.Count() == reportRequest1.NumberofTests);
+            Assert.IsTrue(reportRequest2.TestCases.Count() == reportRequest2.NumberofTests);
+            Assert.IsTrue(reportRequest3.TestCases.Count() == reportRequest3.NumberofTests);
+
+        }
+
+        /// <summary>
+        /// Test Parameters unit tests
+        /// </summary>
+
+        [TestMethod]
+        [DeploymentItem(@"FrameworkTests\JsonTestFiles\ReportRequest_TestParameters_Populates.json")]
+        public void ReportRequest_TestParameters_Populates()
+        {
+            string jsonRequest = ReadJsonFile("ReportRequest_TestParameters_Populates.json");
+
+            ReportRequest reportRequest = new ReportRequest(jsonRequest);
+
+            foreach (TestCase tc in reportRequest)
+            {
+                Assert.IsTrue(tc.TestParameters != null);
+            }
+
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"FrameworkTests\JsonTestFiles\ReportRequest_TestParameters_Missing.json")]
+        public void ReportRequest_TestParameters_Missing()
+        {
+            string jsonRequest = ReadJsonFile("ReportRequest_TestParameters_Missing.json");
+
+            ReportRequest reportRequest = new ReportRequest(jsonRequest);
+
+            foreach (TestCase tc in reportRequest)
+            {
+                Assert.IsTrue(tc.TestParameters == null);
+                Assert.IsTrue(tc.Error == "Test Case is missing Test Parameters, this test will not be included in the report.");
+            }
+        }
+
+
+
         
+
+
+
+
+
 
         public string ReadJsonFile(string path)
         {
@@ -54,5 +344,15 @@ namespace FrameworkTests
             }
             return sr.ReadToEnd();
         }
+
+
+        [TestMethod]
+        [DeploymentItem(@"FrameworkTests\JsonTestFiles\FrameworkTests_ReadJsonFile_ValidFile.json")]
+        public void FrameworkTests_ReadJsonFile_ValidFile()
+        {
+            string jsonFile = ReadJsonFile("FrameworkTests_ReadJsonFile_ValidFile.json");
+            Assert.AreNotEqual(jsonFile, "");
+        }
+
     }
 }
